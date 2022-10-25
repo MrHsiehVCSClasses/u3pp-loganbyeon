47f243075099feb58b3d43d58f9ff2ca0fc817ae
Logan Byeon <loganbyeon@Logans-MacBook-Air.local>
2022-10-25 10:35:04 -0700

    src/main/java/u3pp/Main.java

--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -7,6 +7,6 @@ public class Main
         // Use this space to test your code, or actually run your project
         System.out.println("\n\nHi! This is an empty project.\nYour code will start running from Main.java.\nIt may be daunting to start a project from scratch, but try to have fun with it! :)\n");
 
+        System.out.println(PalindromeTester.isPalindrome("mom"));
     }
-
 }
